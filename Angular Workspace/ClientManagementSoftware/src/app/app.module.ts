01302c591513f1a13b8e7f0a046b0c2291ac3aaf
Shaunak Deodhar <dshaunak@dshaunakXHFQN.vmware.com>
2023-09-06 11:20:45 +0530

    Added frontend components

--- Angular Workspace/ClientManagementSoftware/src/app/app.module.ts
+++ Angular Workspace/ClientManagementSoftware/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateClientComponent } from './create-client/create-client.component';
+import { ScheduleMeetingComponent } from './schedule-meeting/schedule-meeting.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes : Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'createClient', component: CreateClientComponent},
+  {path: 'scheduleMeeting', component: ScheduleMeetingComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateClientComponent,
+    ScheduleMeetingComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular Workspace/ClientManagementSoftware/src/app/create-client/create-client.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-client',
+  templateUrl: './create-client.component.html',
+  styleUrls: ['./create-client.component.css']
+})
+export class CreateClientComponent {
+  emailId = ''
+  name = ''
+  password = ''
+  confirmedPassword = ''
+  address = ''
+
+  constructor(private http : HttpClient) { }
+
+  createClient() {
+    const client = {
+      emailId: this.emailId,
+      name: this.name,
+      password: this.password,
+      address: this.address
+    }
+
+    this.http.post('http://localhost:3000/createClient', client)
+    .subscribe((response:any) => {
+      alert(response.message)
+      this.emailId = '';
+      this.name = '';
+      this.password = '';
+      this.address = '';
+    }, (error) => {
+      console.error('Error in TS for creating client', error)
+    })
+  }
+}
